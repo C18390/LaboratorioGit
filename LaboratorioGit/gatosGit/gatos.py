4dc5d34aeed177cdf7398add2953b871ea3f8a60
C18390 <124937296+C18390@users.noreply.github.com>
2023-02-10 22:52:30 -0600

    Add files via upload
    Se sube la repositorio remoto. Al haber problemas con el comando push se optó por hacerlo manualmente.

---
+++ LaboratorioGit/gatosGit/gatos.py
@@ -0,0 +1,22 @@
+import funcionesTarea
+
+gato1 = funcionesTarea.gato()
+gato1.nombre = "Remy"
+gato1.color = "negro"
+gato1.edad = 4
+gato1.aniadirlista()
+
+gato1 = funcionesTarea.gato()
+gato1.nombre = "Gato3"
+gato1.color = "verde"
+gato1.edad = 12
+gato1.aniadirlista()
+
+
+gato2 = funcionesTarea.gato()
+gato2.nombre = "Pelusa"
+gato2.color = "cafe"
+gato2.edad = 3
+gato2.aniadirlista()
+
+funcionesTarea.printlista()
